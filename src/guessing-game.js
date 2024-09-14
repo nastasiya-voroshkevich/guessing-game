a10fb39c8acf33e49a0958839b61e27bd4aa3e3b
Nastia <the_sun.89@mail.ru>
2024-09-14 17:31:12 +0300

    feat: add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,28 @@
+let number =0
 class GuessingGame {
-    constructor() {}
+  constructor() {
+  
+    }
 
     setRange(min, max) {
+     this.numberMin = min;
+     this.numberMax = max;
 
     }
 
     guess() {
-
+  
+          this.solution =Math.ceil((this.numberMax + this.numberMin) / 2);
+          return this.solution
+     
     }
 
     lower() {
-
+     this.numberMax = this.solution
     }
 
     greater() {
-
+      this.numberMin = this.solution
     }
 }
 
